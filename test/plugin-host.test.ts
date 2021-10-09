a078949e2fddfb75acd93375704fcdde3fa08d4e
Elijah Green <elijahgreen@gmail.com>
2021-10-09 10:22:41 -0700

    Use electron jest runner

--- test/plugin-host.test.ts
+++ test/plugin-host.test.ts
@@ -1,5 +1,4 @@
 import { PluginHost } from '../src/';
-window.MessageChannel = require('worker_threads').MessageChannel;
 
 describe('PluginHost', () => {
   let pluginHost: PluginHost;
@@ -16,5 +15,11 @@ describe('PluginHost', () => {
     pluginHost = new PluginHost('', {});
     const element = document.querySelectorAll('iframe')[0];
     expect(element).not.toBeNull();
+  });
+
+  it('should add only "allow-scripts" sandbox attribute by default', function() {
+    pluginHost = new PluginHost('', {});
+    let element = document.querySelectorAll('iframe')[0];
+    expect(element.sandbox.value).toEqual('allow-scripts');
   });
 });
