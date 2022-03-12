7963ad4de14d10c6ae67c1ea6e82b717e5055251
Elijah Green <elijahgreen@gmail.com>
2022-03-11 16:22:30 -0800

    Update basic example bundler

--- examples/basic-example/server.ts
+++ examples/basic-example/server.ts
@@ -1,24 +1,9 @@
 import express from 'express';
-import path from 'path';
+import cors from 'cors';
 
 const PORT = 3001;
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/index.html'));
-});
-
-app.get('/host.js', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/host.js'));
-});
-
-app.get('/remote.js', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/remote.js'));
-});
-
-app.get('/remote.html', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/remote.html'));
-});
-
+app.use(cors());
+app.use(express.static('dist'));
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
