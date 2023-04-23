cf2c04c48691ed86c722c7a452254a723ab25c86
Elijah Green <elijah@infogata.com>
2023-04-23 15:16:27 -0700

    Update dependencies

--- dts.config.js
+++ dts.config.js
@@ -3,15 +3,15 @@ const path = require('path');
 module.exports = {
   rollup(config, options) {
     config.plugins.push({
-        name: 'whatever',
-        generateBundle(opts, bundle) {
-            const entry = Object.values(bundle).find((chunk) => chunk.isEntry);
-            const file = path.parse(opts.file).base
-            bundle[file].code = bundle[file].code.replace(
-              "'<TEMPLATE>'",
-              JSON.stringify(entry.code)
-            );
-        }
+      name: 'whatever',
+      generateBundle(opts, bundle) {
+        const entry = Object.values(bundle).find((chunk) => chunk.isEntry);
+        const filename = opts.entryFileNames;
+        bundle[filename].code = bundle[filename].code.replace(
+          "'<TEMPLATE>'",
+          JSON.stringify(entry.code)
+        );
+      },
     });
     return config;
   },
